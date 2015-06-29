asm89/selendroid

commit 4afa5ccedc53b0b73319dfe3a6917db0f557fddc

Introduce Predicate and AndroidElement factories
Summary:
Non-native android views sometimes require specific behaviours.
This diff introduce factories for predicates and android elements, allowing test
developers to extend them in order to setup specific behaviours at bootstrap.

Test Plan: Ran with custom views and checked using custom factories worked.

Reviewers: asm

Differential Revision: https://reviews.facebook.net/D33639

selendroid-server/src/main/java/io/selendroid/server/model/ClassPredicate.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.selendroid.server.model;
+
+import android.view.View;
+import com.android.internal.util.Predicate;
+import io.selendroid.server.common.exceptions.NoSuchElementException;
+
+public class ClassPredicate implements Predicate<View> {
+
+  protected final String using;
+
+  public ClassPredicate(String using) {
+    this.using = using;
+  }
+
+  @Override
+  public boolean apply(View to) {
+    try {
+      return Class.forName(using).isInstance(to);
+    } catch (ClassNotFoundException e) {
+      throw new NoSuchElementException("The view class '" + using + "' was not found.");
+    }
+  }
+}

selendroid-server/src/main/java/io/selendroid/server/model/AndroidNativeElementFactory.java
@@ -1,0 +1,27 @@
+/*
+ * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.selendroid.server.model;
+
+import android.view.View;
+import io.selendroid.server.ServerInstrumentation;
+import io.selendroid.server.android.KeySender;
+
+public interface AndroidNativeElementFactory {
+
+  public AndroidNativeElement createAndroidNativeElement(
+      View view,
+      ServerInstrumentation instrumentation,
+      KeySender keys,
+      KnownElements ke);
+}

selendroid-server/src/main/java/io/selendroid/server/model/ContentDescriptionPredicate.java
@@ -1,0 +1,35 @@
+/*
+ * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.selendroid.server.model;
+
+import android.view.View;
+import com.android.internal.util.Predicate;
+
+public class ContentDescriptionPredicate implements Predicate<View> {
+
+  protected final String using;
+
+  ContentDescriptionPredicate(String using) {
+    this.using = using;
+  }
+
+  public boolean apply(View to) {
+    CharSequence contentDescription = to.getContentDescription();
+    if (using == null) {
+      return contentDescription == null;
+    } else {
+      return contentDescription != null && using.contentEquals(contentDescription);
+    }
+  }
+}

selendroid-server/src/main/java/io/selendroid/server/model/DefaultAndroidNativeElementFactory.java
@@ -1,0 +1,30 @@
+/*
+ * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.selendroid.server.model;
+
+import android.view.View;
+import io.selendroid.server.ServerInstrumentation;
+import io.selendroid.server.android.KeySender;
+
+class DefaultAndroidNativeElementFactory implements AndroidNativeElementFactory {
+
+  @Override
+  public AndroidNativeElement createAndroidNativeElement(
+      View view,
+      ServerInstrumentation instrumentation,
+      KeySender keys,
+      KnownElements ke) {
+    return new AndroidNativeElement(view, instrumentation, keys, ke);
+  }
+}

selendroid-server/src/main/java/io/selendroid/server/model/Factories.java
@@ -1,0 +1,46 @@
+/*
+ * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.selendroid.server.model;
+
+
+import io.selendroid.server.util.Preconditions;
+
+public class Factories {
+
+  private static AndroidNativeElementFactory androidNativeElementFactory = null;
+  private static PredicatesFactory predicatesFactory = null;
+
+  public static void set(AndroidNativeElementFactory factory) {
+    androidNativeElementFactory = Preconditions.checkNotNull(factory);
+  }
+
+  public static AndroidNativeElementFactory getAndroidNativeElementFactory() {
+    if (androidNativeElementFactory == null) {
+      set(new DefaultAndroidNativeElementFactory());
+    }
+    return androidNativeElementFactory;
+  }
+
+  public static void set(PredicatesFactory factory) {
+    predicatesFactory = Preconditions.checkNotNull(factory);
+  }
+
+  public static PredicatesFactory getPredicatesFactory() {
+    if (predicatesFactory == null) {
+      set(new PredicatesFactory());
+    }
+    return predicatesFactory;
+
+  }
+}

selendroid-server/src/main/java/io/selendroid/server/model/IdPredicate.java
@@ -1,0 +1,32 @@
+/*
+ * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.selendroid.server.model;
+
+import android.view.View;
+import com.android.internal.util.Predicate;
+import io.selendroid.server.android.ViewHierarchyAnalyzer;
+
+public class IdPredicate implements Predicate<View> {
+
+  protected final String using;
+
+  public IdPredicate(String using) {
+    this.using = using;
+  }
+
+  @Override
+  public boolean apply(View to) {
+    return ViewHierarchyAnalyzer.getNativeId(to).equalsIgnoreCase("id/" + using);
+  }
+}

selendroid-server/src/main/java/io/selendroid/server/model/PartialTextPredicate.java
@@ -1,0 +1,36 @@
+/*
+ * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.selendroid.server.model;
+
+import android.view.View;
+import android.widget.TextView;
+import com.android.internal.util.Predicate;
+import io.selendroid.server.util.SelendroidLogger;
+
+public class PartialTextPredicate implements Predicate<View> {
+
+  protected final String using;
+
+  public PartialTextPredicate(String using) {
+    this.using = using;
+  }
+  public boolean apply(View to) {
+    SelendroidLogger.info("Finding by partial text: " + using);
+    if (to instanceof TextView) {
+      String viewText = ((TextView) to).getText().toString();
+      return viewText.indexOf(using) >= 0;
+    }
+    return false;
+  }
+}

selendroid-server/src/main/java/io/selendroid/server/model/PredicatesFactory.java
@@ -1,0 +1,45 @@
+/*
+ * Copyright 2012-2014 eBay Software Foundation and selendroid committers.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.selendroid.server.model;
+
+
+import android.view.View;
+import com.android.internal.util.Predicate;
+
+public class PredicatesFactory {
+
+  public Predicate<View> createIdPredicate(String using) {
+    return new IdPredicate(using);
+  }
+
+  public Predicate<View> createContentDescriptionPredicate(String using) {
+    return new ContentDescriptionPredicate(using);
+  }
+
+  public Predicate<View> createTextPredicate(String using) {
+    return new TextPredicate(using);
+  }
+
+  public Predicate<View> createPartialTextPredicate(String using) {
+    return new PartialTextPredicate(using);
+  }
+
+  public Predicate<View> createTagNamePredicate(String using) {
+    return new TagNamePredicate(using);
+  }
+
+  public Predicate<View> createClassPredicate(String using) {
+    return new ClassPredicate(using);
+  }
+}

commit 3f1a2a85f01388b44034817203eab9dc5a3f39d4

[server] Fix ClassPredicate
While looking into the failures of `NativeChildElementFindingTest` I
found out that the `ClassPredicate` is throwing an exception in case the
class is not found in the app. This regressed in
4afa5ccedc53b0b73319dfe3a6917db0f557fddc when the class predicate was
extracted to its own class.

I changed the class predicate to return false if the class is not found,
but logging a warning for discoverability. I changed the tests to use an
existing class in the normal case and added new tests to guard for the
appropriate behavior if the class does not exist.

selendroid-server/src/main/java/io/selendroid/server/model/ClassPredicate.java
@@ -16,6 +16,7 @@ package io.selendroid.server.model;
 import android.view.View;
 import com.android.internal.util.Predicate;
 import io.selendroid.server.common.exceptions.NoSuchElementException;
+import io.selendroid.server.util.SelendroidLogger;
 
 public class ClassPredicate implements Predicate<View> {
 
@@ -30,7 +31,8 @@ public class ClassPredicate implements Predicate<View> {
     try {
       return Class.forName(using).isInstance(to);
     } catch (ClassNotFoundException e) {
-      throw new NoSuchElementException("The view class '" + using + "' was not found.");
+      SelendroidLogger.warning("Finding by unknown class " + using);
+      return false;
     }
   }
 }

selendroid-test-app/src/test/java/io/selendroid/nativetests/NativeChildElementFindingTest.java
@@ -33,7 +33,7 @@ public class NativeChildElementFindingTest extends BaseAndroidTest {
   public static final String ACTIVITY_CLASS = "io.testapp." + "HomeScreenActivity";
 
   private void assertListIsEmpty(List<WebElement> elements) {
-    Assert.assertTrue("Expecting empty list when no elements are found.",elements.isEmpty());
+    Assert.assertTrue("Expecting empty list when no elements are found.", elements.isEmpty());
   }
 
 
@@ -177,7 +177,7 @@ public class NativeChildElementFindingTest extends BaseAndroidTest {
     openStartActivity();
     WebElement rootElement = driver().findElement(By.id("l10n"));
     try {
-      rootElement.findElement(By.className("de.dary.MyView"));
+      rootElement.findElement(By.className("io.selendroid.testapp.view.FingerView"));
       Assert.fail("Should not have succeeded");
     } catch (NoSuchElementException e) {
       // this is expected
@@ -186,6 +186,25 @@ public class NativeChildElementFindingTest extends BaseAndroidTest {
 
   @Test()
   public void shouldNotFindChildElementsByClass() {
+    openStartActivity();
+    WebElement rootElement = driver().findElement(By.id("l10n"));
+    assertListIsEmpty(rootElement.findElements(By.className("io.selendroid.testapp.view.FingerView")));
+  }
+
+  @Test()
+  public void shouldNotFindChildElementByClassThatDoesNotExist() {
+    openStartActivity();
+    WebElement rootElement = driver().findElement(By.id("l10n"));
+    try {
+      rootElement.findElement(By.className("de.dary.MyView"));
+      Assert.fail("Should not have succeeded");
+    } catch (NoSuchElementException e) {
+      // this is expected
+    }
+  }
+
+  @Test()
+  public void shouldNotFindChildElementsByClassThatDoesNotExist() {
     openStartActivity();
     WebElement rootElement = driver().findElement(By.id("l10n"));
     assertListIsEmpty(rootElement.findElements(By.className("de.dary.MyView")));