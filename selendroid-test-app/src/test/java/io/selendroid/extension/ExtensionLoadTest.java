ada901634f5bcd8170fe17024728b2e51fc35f35
Alexander Mols <asm@fb.com>
2015-06-29 10:31:48 +0100

    [test-app] Only load extension in ExtensionLoadTest
    `BaseAndroidTest` exposes a way to override the capabalities used to
    start the selendroid server. Use that to only load the extensions in the
    `ExtensionLoadTest`. Other tests don't rely on extensions, so it's
    better if they don't load any.

--- selendroid-test-app/src/test/java/io/selendroid/extension/ExtensionLoadTest.java
+++ selendroid-test-app/src/test/java/io/selendroid/extension/ExtensionLoadTest.java
@@ -2,6 +2,7 @@ package io.selendroid.extension;
 
 import org.junit.Test;
 
+import io.selendroid.common.SelendroidCapabilities;
 import io.selendroid.support.BaseAndroidTest;
 
 import static org.junit.Assert.assertEquals;
@@ -11,5 +12,12 @@ public class ExtensionLoadTest extends BaseAndroidTest {
   public void extensionCallShouldSucceed() {
     assertEquals("I'm an extension!",
         driver().callExtension("io.selendroid.extension.DemoExtensionHandler"));
+  }
+
+  @Override
+  protected SelendroidCapabilities getDefaultCapabilities() {
+    SelendroidCapabilities caps = super.getDefaultCapabilities();
+    caps.setSelendroidExtensions("extension.dex");
+    return caps;
   }
 }
